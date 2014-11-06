d69f4ebf9988492c4a92836c5e76441e8fe5d1da
SVT125 <Jhardjadinata14@gmail.com>
2014-11-06 11:48:25 -0800

    cleaned up code, additional outlier graphing
    indent error

--- researchcorrelation.py
+++ researchcorrelation.py
@@ -1,5 +1,5 @@
 from mpl_toolkits.mplot3d import Axes3D
-from scipy import scipy.stats.stats import pearsonr
+from scipy.stats.stats import pearsonr
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -11,32 +11,38 @@ galaxy_counter = [] # fill in with counters per galaxy
 threshold = 0.01 # wat
 
 def run(threshold: float) -> None:
-        '''Increments galaxy_counter for correlations above threshold for
-        permutations of all variables'''
+        '''Increments galaxy_counter for correlations above threshold for permutations of all variables'''
         for i in range(len(variables)):
                 for j in range(len(variables)):
-                        # increment counter, retrieve vars from indices, calculate corr
-        
+                        print('lelele') # increment counter, retrieve vars from indices, calculate corr
 
-# Will assume returns nothing as it will modify global vars
 def read_data(file_name: str) -> None:
 	''' Reads the data in to list vars x, y, z'''
-        with open(file_name, 'r') as f: #Increases efficiency and removes need for f.close() by auto closing, even if there is an error
-        	lines = f.readlines() # look in docs to loop over all lines
+        with open(file_name, 'r') as f:
+                lines = f.readlines()
         	for line in lines:
-                vars = line.split('\t')
-                	x.append(vars[0])
-                	y.append(vars[1])
-                	z.append(vars[2])
+                        vars = line.split('\t')
+                        x.append(vars[0])
+                        y.append(vars[1])
+                        z.append(vars[2])
 
-# TODO - To take a triple of values instead, won't just be single lined.
 def corr(x: float, y: float) -> float:
         '''Takes a pair of values to return the Pearson correlation.'''
 	return pearsonr(x,y)
 	
-def plot(x:'list of float', y: 'list of float', z: 'list of float') -> None:
+def plot(x:'list of float', y: 'list of float', z: 'list of float', labels=None:'list of bool') -> None:
 	'''Plots the lists xyz in a 3D graph.'''
 	fig = plt.figure()
 	ax = Axes3D(fig)
-	ax.scatter(xs = x, ys = y, zs = z, zdir = 'z', label = 'ys=0, zdir = z')
+	if labels == None:
+                ax.scatter(xs = x, ys = y, zs = z, zdir = 'z', label = 'ys=0, zdir = z')
+        else:   
+                nonanomalies_x = [element for element in x if not x[x.index(element)]]
+                nonanomalies_y = [element for element in y if not y[y.index(element)]]
+                nonanomalies_z = [element for element in z if not z[z.index(element)]]
+                anomalies_x = [element for element in x if x[x.index(element)]]
+                anomalies_y = [element for element in y if y[y.index(element)]]
+                anomalies_y = [element for element in z if z[z.index(element)]]
+                ax.scatter(xs = anomalies_x, ys = anomalies_y, zs = anomalies_z, c = 'red', zdir = 'z', label = 'ys=0, zdir = z')
+                ax.scatter(xs = nonanomalies_x, ys = nonanomalies_y, zs = nonanomalies_z, c = 'blue',zdir = 'z', label = 'ys=0, zdir = z')
 	plt.show()
